69e77a332deee076ecb50a41cb2bceebce91931d
Felix Pujols <pujols.fr@gmail.com>
2020-09-02 20:12:03 -0400

    cors installation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const conectarDB = require('./config/db')
+const cors = require('cors')
 
 // crear servidor
 const app = express();
@@ -9,6 +10,9 @@ app.use(express.json());
 
 // conectar a la base de datos
 conectarDB();
+
+// Habilitar cors
+app.use(cors());
 
 // Habilitar express.json
 app.use(express.json({extended: true}))
