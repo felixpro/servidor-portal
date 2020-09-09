0364b1c8b8aa7b8af492eee42bf6c7728ecd561c
Felix Pujols <pujols.fr@gmail.com>
2020-09-09 00:28:53 -0400

    Production

--- config/db.js
+++ config/db.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
-require('dotenv').config({path: 'variables.env'})
+require('dotenv').config({ path: 'variables.env' });
 
-
-
-const conectarDB = async() => {
-  try {
-    await mongoose.connect(process.env.DB_MONGO, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false
-    })
-    console.log("DB connectada")
-  } catch (error) {
-    console.log(error);
-    process.exit(1); // Detener la app
-  }
+const conectarDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log('DB Conectada');
+    } catch (error) {
+        console.log('hubo un error')
+        console.log(error);
+        process.exit(1); // Detener la app
+    }
 }
 
 module.exports = conectarDB;
--- index.js
+++ index.js
@@ -1,36 +1,30 @@
 
 const express = require('express');
-const conectarDB = require('./config/db')
-const cors = require('cors')
+const conectarDB = require('./config/db');
+const cors = require('cors');
 
-// crear servidor
+// crear el servidor
 const app = express();
-app.use(express.json());
 
-
-// conectar a la base de datos
+// Conectar a la base de datos
 conectarDB();
 
-// Habilitar cors
+// habilitar cors
 app.use(cors());
 
 // Habilitar express.json
-app.use(express.json({extended: true}))
+app.use( express.json({ extended: true }));
+
+// puerto de la app
+const port = process.env.PORT || 4000;
 
 // Importar rutas
-app.use('/api/usuarios', require('./routes/usuarios'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/proyectos', require('./routes/proyectos'))
-app.use('/api/tareas', require('./routes/tareas'))
-
-// Puerto de la app
-const port = process.env.port || 4000;
-
-app.get('/', (req, res) => {
-  res.send("Working")
-})
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/proyectos', require('./routes/proyectos'));
+app.use('/api/tareas', require('./routes/tareas'));
 
 // arrancar la app
 app.listen(port, '0.0.0.0', () => {
-  console.log(`DB connectada ${port}`)
-})
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
